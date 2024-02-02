bb40ff9b35c3d4dbd1b192fdce97ec314cd9c888
lilcryptotech <lilcryptotech@gmail.com>
2024-02-02 08:59:53 -0500

    added readme

--- main.go
+++ main.go
@@ -1,10 +1,33 @@
 package main
-import(
-"fmt"
+
+import (
+	"fmt"
+	"net"
+	
 )
 
 func main() {
-fmt.Println("Welcome IP grabber ")
+	var options string
+	fmt.Print("ENTER OPTIONS: ")
+	fmt.Scan(&options)
 
+	if options == "1" {
+		// Get local IP addresses
+		addrs, err := net.InterfaceAddrs()
+		if err != nil {
+			panic(err)
+		}
 
+		for _, addr := range addrs {
+			ipNet, ok := addr.(*net.IPNet)
+			if ok && !ipNet.IP.IsLoopback() && ipNet.IP.To4() != nil {
+				fmt.Println(ipNet.IP)
+			}
+		}
+	} else {
+		fmt.Println("Invalid command")
+		main() // Exit the program with an error code
+	}
 }
+
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# ip
+
+A simple Go program that retrieves local IP addresses associated with the machine's network interfaces.
+
+## Usage
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/seandadonntech/ip-go
